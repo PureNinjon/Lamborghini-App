408da3ae49de6d16606130eb4303fa7ff6dd45a7
krisztián kőhalmi <krisz0998@gmail.com>
2021-01-13 18:20:53 +0100

    Final

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.IO;
 
+
 namespace Lamborghini_App
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    
     public partial class App : Application
     {
        
